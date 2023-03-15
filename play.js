f703607ed793a41511c932003c96b788b5d7290b
stanw0ng <user.stanley@gmail.com>
2023-03-15 18:20:03 -0400

    refactored play.js and created input.js

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const { connect } = require('./client');
-
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-  });
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
