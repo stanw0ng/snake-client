5d00fe5ef6a83e429f39172fb540c0f638942373
stanw0ng <user.stanley@gmail.com>
2023-03-15 17:30:17 -0400

    experimenting with direction controls in client.js

--- client.js
+++ client.js
@@ -1,15 +1,23 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
     port: 50541 // PORT number here,
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: BOY");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 500)
+  });
+
   conn.on("data", (data) => {
-    console.log('server says: ',data)
-  });  
+    console.log('server says: ',data);
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -19,4 +27,4 @@ const connect = function () {
 
 module.exports = {
   connect
-}
+};
